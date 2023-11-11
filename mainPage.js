c85652d7c9363c4ab6e6a3f58b1cf3ba72dcb67e
S Sai Kiran <saikiran211101@gmail.com>
2023-11-11 10:08:01 +0530

    Homaid Landing Page

---
+++ mainPage.js
@@ -0,0 +1,16 @@
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show");
+        }
+    })
+})
+
+const hiddenEl = document.querySelectorAll(".hidden");
+hiddenEl.forEach((el) => observer.observe(el)); 
